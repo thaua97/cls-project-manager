c2ca20062c1316e38765d76632432c5964e7edfa
Thauã Borges <thaua.borges-ext@merkle.com>
2026-02-03 22:49:29 -0300

    chore: prepare to build

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,6 +4,9 @@ export default defineNuxtConfig({
 	compatibilityDate: '2025-07-15',
 	devtools: { enabled: true },
 	css: ['@/assets/css/main.css'],
+	nitro: {
+		preset: 'node-server'
+	},
 	runtimeConfig: {
 		public: {
 			apiBaseUrl: process.env.NUXT_PUBLIC_API_BASE_URL || 'http://localhost:3333'
